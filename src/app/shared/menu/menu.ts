1991d250c5acec90b8660685c868b90063a6cbe6
KevinFi92 <168017579+KevinFi92@users.noreply.github.com>
2025-10-21 19:49:10 +0200

    resposniv design + reworked header menu
    resposniv design + reworked header menu

--- src/app/shared/menu/menu.ts
+++ src/app/shared/menu/menu.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import {MatMenuModule} from '@angular/material/menu';
 import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
 
 @Component({
   selector: 'app-menu',
-  imports: [MatMenuModule, MatButtonModule],
+  imports: [MatMenuModule, MatButtonModule, MatIconModule],
   templateUrl: './menu.html',
   styleUrl: './menu.scss'
 })
