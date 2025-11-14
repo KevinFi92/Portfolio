33329d2d67420431fb7f9f8ce86a9fbaab5b7a22
KevinFi92 <168017579+KevinFi92@users.noreply.github.com>
2025-11-14 16:58:07 +0100

    changed href to routerlink

--- src/app/landing-page/landing-page.ts
+++ src/app/landing-page/landing-page.ts
@@ -1,11 +1,13 @@
 import {Component} from '@angular/core';
 import {MatButton} from '@angular/material/button';
+import {RouterLink} from '@angular/router';
 
 
 @Component({
   selector: 'app-landing-page',
   imports: [
-    MatButton
+    MatButton,
+    RouterLink
   ],
   templateUrl: './landing-page.html',
   styleUrl: './landing-page.scss'
