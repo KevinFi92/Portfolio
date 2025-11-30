603b404e213a3e468a93d267f46fdd748a214377
KevinFi92 <168017579+KevinFi92@users.noreply.github.com>
2025-11-30 20:07:24 +0100

    Adjust projects margin and improve skills visibility toggle
    Updated the .projects class margin in projects.scss to remove right margin. Enhanced the skills.ts IntersectionObserver to remove the 'visible' class when elements are not intersecting, improving UI responsiveness.

--- src/app/skills/skills.ts
+++ src/app/skills/skills.ts
@@ -21,6 +21,9 @@ export class Skills {
         if (entry.isIntersecting) {
           entry.target.classList.add('visible');
         }
+       else {
+        entry.target.classList.remove('visible');
+      }
       });
     }, {
       threshold: 0.1
